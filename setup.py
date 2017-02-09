6d753ca9f1a49553be8532f9be6258180bc73167
Mohamed Aturban <maturban@Mohameds-MacBook-Pro.local>
2017-02-09 14:40:28 -0500

    init

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = """A simple Python library that can be used push web resources into publi
 
 setup(
     name='archivenow',
-    version='0.0.5',
+    version='0.0.1',
     description=desc,
     long_description=long_description,
     author='Mohamed Aturban',
