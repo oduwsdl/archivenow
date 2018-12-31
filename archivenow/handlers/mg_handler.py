8b0b7d0d8717352942a562efed9f9ca9a3a1c0c6
Mohamed Aturban <maturban@Mohameds-MacBook-Pro.local>
2018-12-30 19:32:35 -0500

    support Python 2/3, and imporove returned Japanese msgs

--- archivenow/handlers/mg_handler.py
+++ archivenow/handlers/mg_handler.py
@@ -1,8 +1,10 @@
+# encoding: utf-8
 import os
 import requests
 
-new_header = 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
 
+
+new_header = 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
 
 class MG_handler(object):
 
@@ -13,10 +15,35 @@ class MG_handler(object):
 
     def push(self, uri_org, p_args=[]):
 
+
+        pre_defined_errors = [     
+            {
+                "We could not acquire this web page because it exceeded the maximum available size ( 30 MB ).":
+                [u"こちらのウェブページはご利用できる最大サイズ", u"を超えたため取得出来ませんでした"]
+            },
+            {
+                "Acquisition under Cookie invalid state is prohibited.":
+                [u"Cookieが無効な状態」での取得は禁止されています。取得確認ページを再読み込み(更新)すると取得できる場"]
+            },
+            {   
+                "Acquisition error occurred.":
+                [u"申し訳ございません、取得エラーが発生しました"]
+            },
+            {
+                "The same URL is acquired within 10 minutes. Please wait at least 10 minutes.":
+                [u"同一のURLが10分以内に取得されています"]
+            },
+            {
+                "We could not acquire it because of unknown error: 503":
+                [u"のため取得出来ませんでした",u"max file size over.",u"申し訳ございません"]
+            }
+        ]
+
         if not uri_org.startswith('http'):
             uri_org = 'http://' + uri_org
 
         msg = ''
+
         try:
 
             headers = {
@@ -24,29 +51,54 @@ class MG_handler(object):
             }
 
             try:
+
                 r = requests.get('http://megalodon.jp/?url=' + uri_org,
                                  headers=headers)
-                token = str(r.content).split('"token" value="',
+                token = r.text.split('"token" value="',
                                              1)[1].split('"',1)[0]
 
                 phpsessid = r.headers['Set-Cookie'].split('PHPSESSID=', 1)[1]
-                phpsessid = phpsessid.split(';', 1)[0]
-
                 cookies = dict(PHPSESSID=phpsessid)
 
             except Exception as e:
                 msg = "Error ({0}): {1}".format(self.name, str(e))
+                pass;
 
-            try:
-                r2 = requests.post('http://megalodon.jp/pc/get_simple/decide',
-                                data={"url":uri_org, "token":token},
-                                cookies=cookies, headers=headers,
-                                )
-            except Exception as e:
-                msg = "Error ({0}): {1}".format(self.name, str(e))
+            if msg == '':
+                try:
+                    r2 = requests.post('https://megalodon.jp/pc/get_simple/decide',
+                                    data={"url":uri_org, "token":token},
+                                    cookies=cookies, headers=headers,
+                                    )
+                except Exception as e:
+                    msg = "Error ({0}): {1}".format(self.name, str(e))
+                    pass;
 
-            msg = str(r2.content).split('location.href = "',
-                                        1)[1].split('"', 1)[0]
+                cont = r2.text
+
+                if msg == '':
+                    try:
+                        msg = cont.split('location.href = "',
+                                                    1)[1].split('"', 1)[0]
+                    except Exception as e:
+                        # try to identify the error from the returned content
+                        err_found = False
+                        for err in pre_defined_errors:
+                            error_key = list(err)[0]
+                            error_descs = err[error_key]
+                            matched = 0
+                            for e_d in error_descs:
+                                if e_d in cont:
+                                    matched = matched + 1
+                                else:
+                                    break;
+                            if matched == len(error_descs):
+                                err_found = True
+                                msg = "Error (" + self.name+ "): "+error_key
+                                break;
+                        if not err_found:
+                            msg = "Error (" + self.name+ "): " + str(e)
+
         except Exception as e:
             if not msg:
                 msg = "Error (" + self.name+ "): " + str(e)
