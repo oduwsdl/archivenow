e2dffc62228353f01be20b740c5144403dc7c1ea
a-mabe <mabe.abby.a@gmail.com>
2020-12-30 16:08:34 -0500

    Refactored the Archive.Today handler to use Selenium.
    Archive.Today added a captcha test when making a POST request to submit a page to be archived. When you manually go to the website and submit a page, there is no captcha. Selenium makes it possible to submit a URL to be archived through the Archive.Today website without encountering a captcha test.

--- archivenow/handlers/is_handler.py
+++ archivenow/handlers/is_handler.py
@@ -1,5 +1,14 @@
 import os
 import requests
+import sys
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
 
 class IS_handler(object):
 
@@ -10,6 +19,64 @@ class IS_handler(object):
 
     def push(self, uri_org, p_args=[], session=requests.Session()):
 
+        msg = ""
+
+        try:
+
+            options = Options()
+            options.headless = False # Run in background
+            driver = webdriver.Firefox(options = options)
+            driver.get("https://archive.is")
+
+            elem = driver.find_element_by_id("url") # Find the form to place a URL to be archived
+
+            elem.send_keys(uri_org) # Place the URL in the input box
+
+            saveButton = driver.find_element_by_xpath("/html/body/center/div/form[1]/div[3]/input") # Find the submit button
+
+            saveButton.click() # Click the submit button
+
+            # After clicking submit, there may be an additional page that pops up and asks if you are sure you want
+            # to archive that page since it was archived X amount of time ago. We need to wait for that page to 
+            # load and click submit again.
+            delay = 30 # seconds
+            try:
+                nextSaveButton = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "/html/body/center/div[4]/center/div/div[2]/div/form/div/input")))
+                nextSaveButton.click()
+
+            except TimeoutException:
+                print("Error")
+
+            # The page takes a while to archive, so keep checking if the loading page is still displayed.
+            loading = True
+            while loading:
+                try:
+                    loadingPage = driver.find_element_by_xpath("/html/body/div/img")
+
+                except:
+                    loading = False
+
+            # After the loading screen is gone and the page is archived, the current URL
+            # will be the URL to the archived page.
+            print(driver.current_url)
+
+            driver.quit()
+
+        except:
+
+            '''
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print((fname, exc_tb.tb_lineno, sys.exc_info() ))
+            '''
+
+            msg = "Unable to complete request."
+
+        return msg
+
+
+
+        '''
         from_heroku = False
         if 'from_heroku' in p_args:
             from_heroku = p_args['from_heroku']
@@ -95,3 +162,4 @@ class IS_handler(object):
                     msg = "Error (" + self.name+ "): " + str(e)
                     pass;
         return msg
+        '''
